d68117bad13a1c419e5ffba118dfb0cf1c1ffbf7
Aleksandr <33430788+apaseka@users.noreply.github.com>
2020-05-27 13:54:52 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AdvertisementDetailComponent } from './advertisement-detail/advertiseme
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// test 6
+// test 1
 
 @NgModule({
   declarations: [
